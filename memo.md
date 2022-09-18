9d290a4b9eb606da6cd4f1317547747d60ca23c8
heeyeonkim <heeyeon3472@gmail.com>
2022-09-18 19:33:40 +0900

    study response

--- memo.md
+++ memo.md
@@ -1,3 +1,10 @@
 > 요청 파라미터 VS HTTP 메시지 바디
 - 요청파라미터 : @RequestParam, @ModelAttribute
 - HTTP 메시지 바디 : @RequestBody
+> HTTP 응답 - 정적 리소스, 뷰 템플릿, HTTP 메시지 사용
+- 정적 리소스 : 웹 브라우저에 정적인 HTML, css, js 를 제공할 때
+- 뷰 템플릿 : 웹 브라우저에 동적인 HTML을 사용할 때
+- Http 메시지 : HTTP API 를 제공하는 경우에 데이터를 JSON 과 같은 형식으로 실어 보낼 때
+
+
+스트링 부트는 '/static', '/public', '/resources', '/META-INF/resources' 디렉토리에서 정적 리소스를 제공함
---
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -0,0 +1,57 @@
+package hello.springmvc.basic.response;
+
+import hello.springmvc.basic.HelloData;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+//@ResponseBody 를 클래스 레벨에 쓰면 아래의 컨트롤러들에 모두 적용이 됨
+//@Controller + @ResponseBody = @RestController 이다
+public class ResponseBodyController {
+
+    @GetMapping("/response-body-string-v1")
+    public void responseBodyV1(HttpServletResponse response) throws IOException {
+        response.getWriter().write("ok");
+    }
+
+    @GetMapping("/response-body-string-v2")
+    public ResponseEntity<String> responseBodyV2(){
+        return new ResponseEntity<>("ok", HttpStatus.OK);
+    }
+
+    @ResponseBody
+    @GetMapping("/response-body-string-v3")
+    public String responseBodyV3(){
+        return "ok";
+    }
+
+    @GetMapping("/response-body-json-v1")
+    public ResponseEntity<HelloData> responseBodyJsonV1(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return new ResponseEntity<>(helloData, HttpStatus.OK);//동적으로 상태를 바꿔야 한다면 ResponseEntity 를 사용하기
+    }
+
+    @ResponseStatus(HttpStatus.OK)//상태를 지정하기 위해 사용하는 애노테이션
+    @ResponseBody
+    @GetMapping("/response-body-json-v2")
+    public HelloData responseBodyJsonV2(){
+        HelloData helloData = new HelloData();
+        helloData.setUsername("userA");
+        helloData.setAge(20);
+
+        return helloData;
+    }
+
+}
